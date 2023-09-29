647ed93a14a734a06e6ae9ea357adf07d494ece7
Wout <hi@wout.codes>
2023-09-29 12:09:52 +0200

    Rewrote the main shrine spec

--- spec/shrine_spec.cr
+++ spec/shrine_spec.cr
@@ -1,44 +1,37 @@
 require "./spec_helper"
 
-Spectator.describe Shrine do
-  include ShrineHelpers
-  include FileHelpers
-
+describe "Shrine" do
   describe ".with_file" do
     context "given a file" do
-      let(exiting_file) { image }
-
       it "yields the given object" do
-        described_class.with_file(exiting_file) do |file|
-          expect(file).to be_a(File)
-          expect(file.closed?).to be_false
-          expect(image.path).to eq(file.path)
+        Shrine.with_file(test_image) do |file|
+          file.should be_a(File)
+          file.closed?.should be_false
+          test_image.path.should eq(file.path)
         end
       end
     end
+  end
 
-    context "given an uploaded file instance" do
-      let(uploaded_file) { uploader.upload(fakeio("uploaded_file")) }
+  context "given an uploaded file instance" do
+    it "downloads the uploaded file" do
+      uploaded_file = uploader.upload(fakeio("uploaded_file"))
 
-      it "downloads the uploaded file" do
-        described_class.with_file(uploaded_file) do |file|
-          expect(file).to be_a(File)
-          expect(file.closed?).to be_false
-          expect(file.gets_to_end).to eq("uploaded_file")
-        end
+      Shrine.with_file(uploaded_file) do |file|
+        file.should be_a(File)
+        file.closed?.should be_false
+        file.gets_to_end.should eq("uploaded_file")
       end
     end
+  end
 
-    context "given an io stream" do
-      let(file_from_io) { fakeio("file_from_io") }
-
-      it "creates and yields a tempfile" do
-        described_class.with_file(file_from_io) do |file|
-          expect(file).to be_a(File)
-          expect(file.closed?).to be_false
-          expect(file.gets_to_end).to eq("file_from_io")
-          expect(file.path).to match(/^#{Dir.tempdir}\/*/)
-        end
+  context "given an io stream" do
+    it "creates and yields a tempfile" do
+      Shrine.with_file(fakeio("file_from_io")) do |file|
+        file.should be_a(File)
+        file.closed?.should be_false
+        file.gets_to_end.should eq("file_from_io")
+        file.path.should match(/^#{Dir.tempdir}\/*/)
       end
     end
   end
